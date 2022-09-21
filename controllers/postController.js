cb674464c8df49574bf1f1ac49d1194d0ce6e169
Dylan King <dwkblue51@yahoo.com>
2022-09-21 16:22:13 -0500

    add post home get controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,1 +1,29 @@
-const mongoose = require("mongoose");
+const Post = require("../models/post");
+const User = require("../models/user");
+const async = require("async");
+
+exports.postHomeGet = (req, res, next) => {
+  if (req.user) {
+    async.parallel(
+      {
+        user(callback) {
+          User.findOne({ username: req.user.username }).exec(callback);
+        },
+        posts(callback) {
+          Post.find().exec(callback);
+        },
+      },
+      (err, results) => {
+        if (err) {
+          return next(err);
+        }
+        res.render("posts", {
+          user: results.user,
+          posts: results.post,
+        });
+      }
+    );
+  } else {
+    res.redirect("/");
+  }
+};
